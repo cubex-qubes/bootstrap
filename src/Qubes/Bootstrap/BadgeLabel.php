a764269c7018d138a284dc37aff3a5690f9bb170
Chris <chris@seemly.co.uk>
2013-05-14 12:33:18 +0100

    Bootstrap helper created for badges and labels. Example code can be found in views/examples.php

---
+++ src/Qubes/Bootstrap/BadgeLabel.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * @author  chris.sparshott
+ */
+
+namespace Qubes\Bootstrap;
+
+use Cubex\View\HtmlElement;
+
+class BadgeLabel extends BootstrapItem
+{
+  protected $_text;
+  protected $_element;
+  protected $_style;
+
+  const ELEMENT_LABEL = 'label';
+  const ELEMENT_BADGE = 'badge';
+
+  const STYLE_DEFAULT   = 'default';
+  const STYLE_SUCCESS   = '-success';
+  const STYLE_WARNING   = '-warning';
+  const STYLE_IMPORTANT = '-important';
+  const STYLE_INFO      = '-info';
+  const STYLE_INVERSE   = '-inverse';
+
+  public function __construct($text = '', $element = self::ELEMENT_LABEL, $style = self::STYLE_DEFAULT)
+  {
+    $this->_text  = $text;
+    $this->_element = $element;
+    $this->_style = $style;
+  }
+
+  public function setText($text)
+  {
+    $this->_text = $text;
+    return $this;
+  }
+
+  public function getText()
+  {
+    return $this->_text;
+  }
+
+  public function setElement($element)
+  {
+    $this->_element = $element;
+    return $this;
+  }
+
+  public function getElement()
+  {
+    return $this->_element;
+  }
+
+  public function setStyle($style = self::STYLE_DEFAULT)
+  {
+    $this->_style = $style;
+    return $this;
+  }
+
+  public function getStyle()
+  {
+    return $this->_style;
+  }
+
+  public function render()
+  {
+    $out = new HtmlElement(
+      'span',
+      ['class' => $this->_element . ' ' . $this->_element . $this->_style],
+      $this->_text
+    );
+    return $out->__toString();
+  }
+}
